d10b3eb6beb828730e1369cb0738a0c3d90633a7
theGaryLarson <gary.larson316@gmail.com>
2023-02-01 00:53:30 -0800

    Some basic examples showing how to load files and select data to insert. Need to look into cursor on slides but done for the night.

--- snippets/bendover_procedure_example.sql
+++ snippets/bendover_procedure_example.sql
@@ -1,8 +1,17 @@
+
+# In this example, /path/to/bendover_data_feed.csv is the file path to the CSV file,
+# and targeted_marketing is the name of the table that the data will be loaded into.
+# The FIELDS TERMINATED BY and ENCLOSED BY clauses specify the delimiter and enclosure
+# characters used in the CSV file. The LINES TERMINATED BY clause specifies the end-of-line
+# character used in the file. The IGNORE 1 ROWS clause tells MySQL to ignore the first row in
+# the file, which is typically the header row.
+
 USE targeted_marketing;
 DELIMITER //
 CREATE PROCEDURE parse_csv()
 BEGIN
-  LOAD DATA INFILE 'bendover_data_feed.csv'
+  DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET @file_not_found = 1;
+  LOAD DATA INFILE 'py/bendover_data_feed.csv'
   INTO TABLE targeted_marketing
   FIELDS TERMINATED BY ','
   ENCLOSED BY '"'
@@ -12,5 +21,9 @@ BEGIN
    religious_affiliation, religious_intensity, social_issue_views, view_intensity,
    social_mate_preference, gender
   );
+
+  IF @file_not_found THEN
+      SELECT 'File not found';
+  END IF;
 END//
 DELIMITER ;
--- snippets/individual_fields_from_csv.sql
+++ snippets/individual_fields_from_csv.sql
@@ -1,13 +1,26 @@
-DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET @file_not_found = 1;
 
-LOAD DATA INFILE '/path/to/bendover_data_feed.csv'
-INTO TABLE targeted_marketing
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(first_name, last_name);
+# This will load the data from the file /path/to/bendover_data_feed.csv and insert
+# only the first name and last name into the table targeted_marketing. The fields
+# in the csv file are separated by commas, and values are enclosed by double quotes.
+# The first row of the csv file will be ignored (the header), and the values from each
+# subsequent row will be inserted into the table.
 
-IF @file_not_found THEN
-  SELECT 'File not found';
-END IF;
+DELIMITER //
+CREATE PROCEDURE load_bendover_data()
+BEGIN
+  DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET @file_not_found = 1;
+
+  LOAD DATA INFILE 'py/bendover_data_feed.csv'
+  INTO TABLE targeted_marketing
+  FIELDS TERMINATED BY ','
+  ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  IGNORE 1 ROWS
+  (first_name, last_name);
+
+  IF @file_not_found THEN
+    SELECT 'File not found';
+  END IF;
+END//
+
+DELIMITER ;
