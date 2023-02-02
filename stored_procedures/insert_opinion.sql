3e5ba051ec3ef7e387274614435767a1fbe9cacb
E. Brink <80282265+brinkbrink@users.noreply.github.com>
2023-02-02 10:32:51 -0800

    rough draft

--- stored_procedures/insert_opinion.sql
+++ stored_procedures/insert_opinion.sql
@@ -7,7 +7,9 @@ BEGIN
     DECLARE not_found = FALSE;
 DECLARE CONTINUE HANDLER FOR NOT FOUND
     SET not_found = TRUE;
-INSERT INTO opinion (opinion, intensity) VALUES ('from csv', 'from csv');
+DECLARE opinion VARCHAR;
+DECLARE intensity INT;
+INSERT INTO opinion (opinion, intensity) VALUES (opinion, intensity);
 INSERT INTO sm_opinion_type (type) VALUES ('from csv');
 IF not_found = TRUE THEN
     SELECT 'Values not inserted - no values found.' AS message;
