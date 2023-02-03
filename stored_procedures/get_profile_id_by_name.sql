c5a4e5aa337f7af1ad2c00cec2d6520923bcc527
theGaryLarson <gary.larson316@gmail.com>
2023-02-02 20:17:47 -0800

    changed this to a function just to practice with return values

--- stored_procedures/get_profile_id_by_name.sql
+++ stored_procedures/get_profile_id_by_name.sql
@@ -1,11 +1,15 @@
 USE targeted_marketing;
 DELIMITER //
-CREATE PROCEDURE get_profile_id (IN first_name VARCHAR(255), IN last_name VARCHAR(255))
+CREATE FUNCTION get_profile_id (first_name VARCHAR(255), last_name VARCHAR(255))
+RETURNS INT
+DETERMINISTIC READS SQL DATA
 BEGIN
-  SELECT id
+  DECLARE result INT;
+  SELECT id INTO result
   FROM targeted_marketing.core_profile
   WHERE f_name = first_name AND l_name = last_name;
+  RETURN result;
 END//
-
 DELIMITER ;
-CALL get_profile_id("John", "Doe");
+
+SELECT get_profile_id("John", "Doe") as "Profile ID";
