3096791418499121b629038319da7ac7bb9b430b
theGaryLarson <gary.larson316@gmail.com>
2023-02-04 18:59:27 -0800

    line 13. Was attempting to insert values into a table without declaring fields.

--- stored_procedures/insert_profile_opinion.sql
+++ stored_procedures/insert_profile_opinion.sql
@@ -10,7 +10,7 @@ CREATE PROCEDURE insert_profile_opinion
 # fixme: look at IN OUT keywords that way can return flag if exists already to handle transaction
 BEGIN
     IF NOT check_exists_profile_opinion(core_profile_id_arg, opinion_id_arg) THEN
-        INSERT INTO profile_opinion
+        INSERT INTO profile_opinion (core_id, opinion_id, date, intensity)
             VALUES (core_profile_id_arg, opinion_id_arg, date_arg, intensity_arg);
         # return true if is inserted
     # ELSE
@@ -18,3 +18,7 @@ BEGIN
     END IF;
 END//
 DELIMITER ;
+
+CALL insert_profile_opinion(15, 15, CURDATE(), 5);
+SELECT * FROM profile_opinion;
+DELETE FROM profile_opinion WHERE `date` = CURDATE();
