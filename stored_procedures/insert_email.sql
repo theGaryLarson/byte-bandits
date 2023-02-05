d33f5979ce0b47794be8a680780ff15d4b49e76e
theGaryLarson <gary.larson316@gmail.com>
2023-02-04 18:58:49 -0800

    wrong variable was being read. changed email argument name to email_var: Resolved

--- stored_procedures/insert_email.sql
+++ stored_procedures/insert_email.sql
@@ -2,12 +2,15 @@ USE targeted_marketing;
 DELIMITER //
 CREATE PROCEDURE insert_email
 (
-	profile_id		INT,
-    email			VARCHAR(255)
+	profile_id_var		INT,
+    email_var			VARCHAR(255)
 )
 BEGIN
-IF NOT check_exists_email(email) THEN
-	INSERT INTO email
-	VALUE(profile_id, email);
+IF NOT check_exists_email(email_var) THEN
+	INSERT INTO email (core_id, email)
+	VALUES(profile_id_var, email_var);
 END IF;
-END
+END//
+DELIMITER ;
+
+CALL insert_email(15 ,'gary.larson@seattlecolleges.edu');
