135b57d9987a7d27bd7d7f22712605b13966f7ce
theGaryLarson <gary.larson316@gmail.com>
2023-02-04 16:21:47 -0800

    80% complete. Driver program to load all tables from bendover_data_feed table

--- stored_procedures/driver.sql
+++ stored_procedures/driver.sql
@@ -30,9 +30,10 @@ DECLARE curr_occupation						VARCHAR(144);
 DECLARE curr_hobbies						VARCHAR(200);
 
 -- fields needed for inserts into tables
-DECLARE platform_id						INT;
-DECLARE curr_profile_id					INT;
-DECLARE curr_opinion_id					INT;
+DECLARE platform_id						INT; # BENDOVER
+DECLARE curr_opinion_type_id			INT; # POLITICS, CLIMATE-CHANGE, RELIGION, etc.
+DECLARE curr_profile_id					INT; # CORE PROFILE PRIMARY KEY
+DECLARE curr_opinion_id					INT; # OPINION ID TO BE USED IN PROFILE_OPINION SO AN INTENSITY CAN BE ASSOCIATED
 
 DECLARE cur CURSOR FOR
 SELECT *
@@ -46,7 +47,7 @@ WHERE platform = "BENDOVER";
 SELECT platform_id;
 
 # populate core profile names first to generate needed primary keys for FK dependencies
--- todo: this method handles duplicates and aliases
+-- todo: this method will handle duplicates and aliases ... eventually
 CALL insert_bendover_names_into_core_profile();
 
 OPEN cur;
@@ -71,23 +72,31 @@ insertion_loop: LOOP
                     curr_education,
                     curr_occupation,
                     curr_hobbies;
-	###################################################################################            
-	# main work is done here at each step will insert data into the appropriate table #
-    ###################################################################################
+                    
+	####################################################################################################            
+	# main work is done here at each pass through the loop will insert data into the appropriate table #
+    ####################################################################################################
     
     -- retrieve current profile_id
     SELECT get_profile_id_by_name(curr_f_name, curr_l_name)
     INTO curr_profile_id;
     
-    -- insert email from current bendover_data_feed record
+    -- insert into email table from current bendover_data_feed record
     CALL insert_email(curr_profile_id, curr_email);
     
     -- todo: insert political affiliation and political intensity into profile_opinion
-		-- use hardcoded sm_opinion_type.type
-        -- get opinion_id from opinion table. if doesn't exist enter then return id
-        -- use insert into profile_opinion(core_id, opinion_id, date, intensity) --> (curr_profile_id, curr_opinion_id, CURDATE(), )
-        -- todo: transaction assignment - add some logic to see if there already is a profile opinion for this opinion_id if
-        -- so transfer current to transaction table and insert new if intensity has changed
+	-- use hardcoded sm_opinion_type.type
+    -- get opinion_id from opinion table. if doesn't exist enter then return id
+    -- use insert into profile_opinion(core_id, opinion_id, date, intensity) --> (curr_profile_id, curr_opinion_id, CURDATE(), )
+    -- todo: transaction assignment - add some logic to see if there already is a profile opinion for this opinion_id if
+    -- so transfer current to transaction table and insert new if intensity has changed
+        
+    -- store the opinion type for the current political_affiliation in curr_opinion_type_id
+    -- adds 'Politics' if not found then returns the id. (have to be really careful about typos)
+    SELECT get_current_opinion_type_id('Politics')
+    INTO curr_opinion_type_id;
+        
+    SELECT get_opinion_id(curr_opinion_type_id, curr_political_affil) INTO curr_opinion_id;
     -- leave loop if next item is not found
     IF @done THEN
 		LEAVE insertion_loop;
