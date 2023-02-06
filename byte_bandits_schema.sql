d1727a53a9329c033bd85b018aca306803ff2241
theGaryLarson <gary.larson316@gmail.com>
2023-02-05 16:36:41 -0800

    DROP SCHEMA changed to DROP DATABASE to clear functions and procedures

--- byte_bandits_schema.sql
+++ byte_bandits_schema.sql
@@ -7,7 +7,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema targeted_marketing
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `targeted_marketing` ;
+DROP DATABASE IF EXISTS `targeted_marketing` ;
 
 -- -----------------------------------------------------
 -- Schema targeted_marketing
