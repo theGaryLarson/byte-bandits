a9b4cd3e45ca771ea02c3c55b733491249ec0d6f
theGaryLarson <gary.larson316@gmail.com>
2023-02-05 16:40:17 -0800

    changed from LOOP to WHILE. Added hack to fix short circuiting on loop when inner select statements return null

--- stored_procedures/insert_bendover_names_into_core_profile.sql
+++ stored_procedures/insert_bendover_names_into_core_profile.sql
@@ -2,32 +2,47 @@ USE targeted_marketing;
 DELIMITER //
 CREATE PROCEDURE insert_bendover_names_into_core_profile()
 BEGIN
-	DECLARE f_name_var VARCHAR(45);
-	DECLARE l_name_var VARCHAR(45);
-	DECLARE cur CURSOR
-    FOR SELECT first_name, last_name
-    FROM bendover_data_feed;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET @done = TRUE;
+	DECLARE f_name_var 		VARCHAR(45);
+	DECLARE l_name_var 		VARCHAR(45);
+    DECLARE row_not_found 	TINYINT DEFAULT FALSE;
+    DECLARE present_id		INT;
+    
+	DECLARE cur CURSOR FOR
+		SELECT first_name, last_name
+		FROM bendover_data_feed;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND
+		SET row_not_found = TRUE;
     OPEN cur;
-    insertion_loop: LOOP
+    
+    WHILE row_not_found = FALSE DO
 		FETCH cur INTO f_name_var, l_Name_var;
-        # question - Would it be more optimal to use another cursor than this function which returns every name on an individual query?
-		IF get_profile_id_by_name(f_name_var, l_name_var) IS NULL THEN
+        SELECT id
+        INTO present_id
+        FROM core_profile
+        WHERE f_name = f_name_var AND l_name = l_name_var;
+        
+		IF present_id IS NULL THEN
 			INSERT INTO core_profile(f_name, l_name)
 			VALUES (f_name_var, l_name_var);
+            # fixme: hack solution to prevent short circuiting of loop iteration
+            SET row_not_found = FALSE;
 		#ELSE
 			-- todo: handle people with same name and aliases
 			-- leverage bendover tables unique bendover_id, url of social media table and/or email?
 		END IF;
-        IF @done THEN
-			LEAVE insertion_loop;
-        END IF;
-	END LOOP;
+	END WHILE;
         CLOSE cur;
 END//
 
 DELIMITER ;
+
+# troubleshooting statements...
 CALL insert_bendover_names_into_core_profile();
 SELECT get_profile_id_by_name('Michael', 'Brown') AS 'id', f_name, l_name
 FROM core_profile
 WHERE f_name = 'Michael' AND l_name = 'Brown';
+DROP PROCEDURE IF EXISTS insert_bendover_names_into_core_profile;
+SELECT id
+	FROM core_profile
+	WHERE f_name = 'Jane' AND l_name = 'Doe';
+DELETE FROM core_profile WHERE f_name = 'Jane' AND l_name = 'Doe';
