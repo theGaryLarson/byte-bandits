2731dbe47f13abf57b5b9b24eaf5d0efb01b56e5
theGaryLarson <gary.larson316@gmail.com>
2023-02-05 18:05:36 -0800

    added some todo's for transaction assignment

--- stored_procedures/driver.sql
+++ stored_procedures/driver.sql
@@ -13,20 +13,15 @@ DECLARE curr_political_affil				VARCHAR(45);
 DECLARE curr_political_intensity			INT;
 DECLARE curr_religious_affil				VARCHAR(45);
 DECLARE curr_religious_intensity			INT;
--- reminder : json object
 DECLARE curr_social_issue_views				VARCHAR(200);
--- reminder : json object
 DECLARE curr_social_issue_date				VARCHAR(200);
--- reminder : json object
 DECLARE curr_social_issue_view_intensity	VARCHAR(200);
--- reminder : json object
 DECLARE curr_social_mate_pref				VARCHAR(200);
 DECLARE curr_gender							VARCHAR(3);
 DECLARE curr_age							INT;
 DECLARE curr_location						VARCHAR(45);
 DECLARE curr_education						VARCHAR(144);
 DECLARE curr_occupation						VARCHAR(144);
--- reminder : json object
 DECLARE curr_hobbies						VARCHAR(200);
 
 -- fields needed for inserts into tables
@@ -38,10 +33,11 @@ DECLARE curr_opinion_id					INT; # OPINION ID TO BE USED IN PROFILE_OPINION SO A
 DECLARE cur CURSOR FOR
 SELECT *
 FROM bendover_data_feed;
+
 DECLARE CONTINUE HANDLER FOR NOT FOUND
     SET @done = TRUE;
 
-# intialize variables that will be used for scope of driver call
+# initialize variables that will be used for scope of driver call
 # todo: create a method that checks if platform exists, if not adds it then returns PK
 SELECT id INTO platform_id
 FROM social_media_platform
@@ -84,11 +80,13 @@ insertion_loop: LOOP
     
     -- insert into email table from current bendover_data_feed record
     CALL insert_email(curr_profile_id, curr_email);
-    
-    -- insert political affiliation and political intensity into profile_opinion
-    -- store the opinion type for the current political_affiliation in curr_opinion_type_id
-    -- adds 'Politics' if not found then returns the id. (have to be really careful about typos)
+
+
+	# select politic category ----------------------------------------------------------------
+	-- insert political affiliation and political intensity into profile_opinion
+	-- adds 'Politics' if not found then returns the id. (have to be really careful about typos)
     SELECT get_current_opinion_type_id('Politics')
+    -- store the opinion type for the current political_affiliation in curr_opinion_type_id
     INTO curr_opinion_type_id;
 
     SELECT get_opinion_id(curr_opinion_type_id, curr_political_affil)
@@ -103,9 +101,9 @@ insertion_loop: LOOP
         CURDATE(),
         curr_political_intensity
 	);
+    # ----------------------------------------------------------------------------------------
 
-    -- todo: insert religious affiliation data
-	-- change to religion category
+	-- change to religion category -----------------------------------------------------------
     SELECT get_current_opinion_type_id('Religion')
     INTO curr_opinion_type_id;
 
@@ -122,6 +120,20 @@ insertion_loop: LOOP
         CURDATE(),
         curr_religious_intensity
 	);
+	# ----------------------------------------------------------------------------------------
+
+	-- fixme: transaction assignment - add some logic to see if there already is a profile opinion for this opinion_id
+	--  if so transfer current record to transaction table and insert new intensity & date (even if intensity is the same)
+	# todo: social issue views       (json)
+	# todo: social issue post date   (json)
+	# todo: social issue intensity   (json)
+
+	# todo: gender
+	# todo: age
+	# todo: location
+	# todo: education
+	# todo: occupation
+	# todo: hobbies                  (json)
     -- leave loop if next item is not found
     IF @done THEN
 		LEAVE insertion_loop;
