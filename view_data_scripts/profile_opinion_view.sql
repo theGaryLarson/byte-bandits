01151c8960b9d9567fe89fda6b5941bbdd4142d3
theGaryLarson <gary.larson316@gmail.com>
2023-02-05 20:44:39 -0800

    renamed from view_profile_opinions.sql

--- view_data_scripts/profile_opinion_view.sql
+++ view_data_scripts/profile_opinion_view.sql
@@ -1,5 +1,5 @@
 USE targeted_marketing;
-CREATE VIEW profile_opinions AS
+CREATE VIEW profile_opinion_view AS
 SELECT profile_opinion.id AS 'Core id', f_name, l_name, sm_opinion_type.type AS 'Category', opinion_id, opinion.opinion, intensity, `date` AS 'Post-date'
 FROM targeted_marketing.profile_opinion
 JOIN core_profile
@@ -9,3 +9,5 @@ ON opinion.id = opinion_id
 JOIN sm_opinion_type
 ON opinion.smo_type_id = sm_opinion_type.id
 ORDER BY category, intensity DESC, l_name, f_name;
+
+SELECT * FROM profile_opinion_view;
