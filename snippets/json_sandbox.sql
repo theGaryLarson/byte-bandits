b09e50668ee5a8845fb653f525a33b858e706b64
theGaryLarson <gary.larson316@gmail.com>
2023-02-07 19:26:21 -0800

    updated alias to immigration

--- snippets/json_sandbox.sql
+++ snippets/json_sandbox.sql
@@ -7,7 +7,7 @@ DECLARE json_string	VARCHAR(255);
 # need to iterate over because more than one. (USE CURSOR)
 SELECT concat('[', social_issue_views, ']') INTO json_string FROM bendover_data_feed LIMIT 1;
 
-SELECT JSON_EXTRACT(json_string, '$[0].immigration') AS 'climate change';
+SELECT JSON_EXTRACT(json_string, '$[0].immigration') AS 'immigration';
 END//
 DELIMITER ;
 CALL json_convert();
