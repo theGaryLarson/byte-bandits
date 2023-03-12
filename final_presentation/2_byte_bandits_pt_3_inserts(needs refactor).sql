b9d57d9e59b4e40a518acb158481cdda0a9dfaca
theGaryLarson <gary.larson316@gmail.com>
2023-03-11 22:21:21 -0800

    Robel, made quite a bit of changes to these inserts. Check them out.

--- final_presentation/2_byte_bandits_pt_3_inserts(needs refactor).sql
+++ final_presentation/2_byte_bandits_pt_3_inserts(needs refactor).sql
@@ -35,24 +35,13 @@ VALUES
     ('Bigender', 'B');
 SELECT * FROM gender_look_up;
 
-
-INSERT INTO profile_data (core_id, gender_id, ethnicity, birthdate, marital_status)
-VALUES (11, 2, 'Caucasian', '1987-05-23', 'Single'),
-       (12, 1, 'African American', '1993-09-12', 'Married'),
-	   (13, 2, 'Asian', '1978-03-15', 'Divorced'),
-	   (14, 1, 'Hispanic', '1989-07-20', 'Widowed');
-
-
-# todo: maybe the updated version since erd contains education & occupation ^
-# fixme: missing gender_id, can't find starting pk value in gender_lookup table
-    # used same gender_id as above profile_data, not sure if it's right since there's repeated gender_id's
--- INSERT INTO profile_data (core_id, gender_id, ethnicity, birthdate, marital_status, education, occupation)
--- VALUES (11, 2, 'Caucasian', '1987-05-23', 'Single', 'Bachelor's degree in Computer Science', 'Software Engineer'),
---        (12, 1, 'African American', '1993-09-12', 'Married', 'Associate's degree in Business', 'Entrepreneur'),
--- 	   (13, 2, 'Asian', '1978-03-15', 'Divorced', 'Master's degree in Education', 'Teacher'),
--- 	   (14, 1, 'Hispanic', '1989-07-20', 'Widowed', 'Doctorate degree in Physics', 'Research Scientist'),
--- 	   (15, 2, 'Hispanic', '1989-07-20', 'Widowed', 'Bachelor's degree in Computer Science', 'Software Engineer');
-
+# todo: Robel we need to add these. Education and Occupation tables are for past schooling and jobs
+INSERT INTO profile_data (core_id, gender_id, ethnicity, birthdate, marital_status, most_recent_education, current_occupation)
+VALUES (11, 12, 'Caucasian', '1987-05-23', 'Single', 'Bachelor\'s degree in Computer Science', 'Software Engineer'),
+       (12, 11, 'African American', '1993-09-12', 'Married', 'Associate\'s degree in Business', 'Entrepreneur'),
+	   (13, 12, 'Asian', '1978-03-15', 'Divorced', 'Master\'s degree in Education', 'Teacher'),
+	   (14, 11, '', '1989-07-20', 'Widowed', 'Doctorate degree in Physics', 'Research Scientist'),
+       (15, 12, 'Caucasian', '1981-10-04', 'Single', 'Bachelor\'s degree in Computer Science', 'Software Engineer');
 
 # todo: create as view
 SELECT f_name, l_name, gender_id, gender_look_up.acronym FROM core_profile
@@ -92,53 +81,66 @@ SELECT * FROM phone;
 
 
 # fixme: should hobby_name be array entries or should I just pick one hobby for each entry
-# INSERT INTO hobby_look_up (hobby_name)
-# VALUES("[''Programming'', ''Reading'', ''Hiking'']"),
-#     ("[''Surfing'', ''Yoga'', ''Cooking'']"),
-#     ("['Painting', 'Traveling', 'Gardening']"),
-#     ("['Astronomy, Sudoku, Beer Pong']"),
-#     ("['Programming', 'Reading', 'Hiking']");
-
-INSERT INTO hobby_lookup (hobby_name)
-VALUES('Programming'),
-    ('Yoga'),
+# todo: Robel, it is an array of hobbies. But people can have more than one hobby. We receive a list of hobbies from a data feed for each profile
+INSERT INTO hobby_look_up (hobby_name)
+VALUES ('Astronomy'),
+    ('Beer Pong'),
+    ('Cooking'),
+    ('Gardening'),
+    ('Hiking'),
     ('Painting'),
+    ('Programming'),
+    ('Reading'),
     ('Sudoku'),
-    ('Reading');
+    ('Surfing'),
+    ('Traveling'),
+    ('Yoga');
 
-INSERT INTO time_spent_on_hobby_lookup (frequency)
-VALUES('Daily'),
-      ('Weekly'),
-      ('Weekdays'),
-      ('Weekends');
+# fixme: I would not have a time spent on hobby. I would have the definition of hobby for our use be something like more than 10 hrs/week
+#  then if the topic meets that requirement insert it into this table to get an id to use as a FK in profile_hobby table
+# INSERT INTO time_spent_on_hobby_look_up (time_spent)
+# VALUES('Daily'),
+#       ('Weekly'),
+#       ('Weekdays'),
+#       ('Weekends');
 
-INSERT INTO occupation (core_id, occupation)
-VALUES (11, 'Software Engineer'),
-       (12, 'Entrepreneur'),
-       (13, 'Teacher'),
-       (14, 'Research Scientist'),
-       (15, 'Software Engineer');
+# todo: Robel these aren't a list of occupations. Occupation may be a good look up but this was used for transactional data (archiving)
+# INSERT INTO past_occupation (core_id, job_title)
+# VALUES (11, 'Software Engineer'),
+#        (12, 'Entrepreneur'),
+#        (13, 'Teacher'),
+#        (14, 'Research Scientist'),
+#        (15, 'Software Engineer');
 
 # todo: can't seem to find fk id's in schema for --> gender_id, political_affiliation_look_up_id, religious_affiliation_look_up_id
 # fixme: need fk id's for --> gender_id, political_affiliation_look_up_id, religious_affiliation_look_up_id
- INSERT INTO social_mate_preference (core_id, gender_id, political_affiliation, religious_affiliation)
- VALUES (11, , 'Liberal', 'Atheist'),
- 	     (12, , 'Conservative', 'Christian'),
-        (13, , 'Independent', 'Buddhist'),
-        (14, , 'Conservative', 'Jewish');
+# todo: Robel these were not part of the first 5 rows of data because they were not part of the assignment.
+
+#  INSERT INTO social_mate_preference (core_id, gender_id, political_affiliation_look_up_id, religious_affiliation_look_up_id)
+#  VALUES (11, 12, 'Liberal', 'Atheist'),
+#  	     (12, 11, 'Conservative', 'Christian'),
+#         (13, 12, 'Independent', 'Buddhist'),
+#         (14, 11, 'Conservative', 'Jewish'),
+#         (15, 12, 'Liberal', 'Christian');
 
 
-# todo: can't seem to find fk id's in schema for --> hobby_id
-    # what is the hobby_id star at if there's no auto_increment value given? does it start at 1?
-# fixme: need fk id's for --> hobby_id
-INSERT INTO profile_hobby (core_id, hobby_id)
+# todo: can't seem to find fk id's in schema for --> hobby_id # fixme: need to add the hobbies to the hobby_look_up table then use a SELECT * FROM hobby_look_up to view the ids to enter
+    # what is the hobby_id start at if there's no auto_increment value given? does it start at 1? A: Sometimes but I've noticed it is not consistent.
+# fixme: need fk id's for --> hobby_id there isn't any hobbies listed. That wasn't developed until a later assignment.
+#  the manual inserts did not have any hobbies in the csv file.
+INSERT INTO profile_hobby (core_id, hobby_id, weekly_time_spent)
 VALUES
-    (11, ),
-    (12, ),
-    (13, ),
-    (14, ),
-    (15, ),
-    (16, );
+    (11, 11, 10),
+    (11, 14, 8),
+    (12, 12, 9),
+    (12, 15, 15),
+    (13, 20, 5),
+    (13, 21, 5),
+    (14, 13, 15),
+    (14, 14, 5),
+    (15, 17, 25),
+    (15, 13, 5);
+
 
        
 ##########################################
@@ -262,33 +264,30 @@ VALUES ( 11, 17, 10),
 --        (15, 15, 4, '2022-04-21');
 
 
-
+# todo: Robel, you had entered Atheist 2x. This is a look up table meant to hold unique values
 INSERT INTO religious_affiliation_look_up (affiliation)
 VALUES ('Atheist'),
        ('Christian'),
        ('Buddhist'),
-       ('Jewish'),
-       ('Atheist');
+       ('Jewish');
+#        ('Atheist');
 
+# todo: checked the schema to find starting value for religious_affiliation_look_up_id auto-increment, but there wasn't one??
+# todo: Robel can't remember but I'm sure I didn't have religious affiliations associated with the first 5 rows of data.
+# religious affiliations wasn't required until a later assignment
+INSERT INTO core_profile_has_religious_affiliation (religious_affiliation_look_up_id, core_profile_id)
+VALUES (2, 15);
 
 # todo: checked the schema to find starting value for core_profile_religious_affiliation auto-increment, but there wasn't one??
 # todo: would auto-increment value start at 1 then??
 # fixme: timestamp dates are examples, couldn't find actual dates in clean_bendover_data_feed.csv
-INSERT INTO religious_transaction (core_profile_religious_affiliation, `timestamp`, prev_intensity)
-VALUES (, '2022-10-04', 1),
-       (, '2022-12-31', 10),
-       (, '2023-02-04', 7),
-       (, '2023-02-14', 8),
-       (, '2023-03-02', 4);
-
-
-# todo: checked the schema to find starting value for religious_affiliation_look_up_id auto-increment, but there wasn't one??
-INSERT INTO core_profile_religious_affiliation (religious_affiliation_look_up_id, core_profile_id)
-VALUES (, 11),
-       (, 12),
-       (, 13),
-       (, 14),
-       (, 15);
+# todo: robel, these do not exist. This is a table to store the old data as new data comes in.
+# INSERT INTO religious_transaction (core_profile_has_religious_affiliation_id, `timestamp`, prev_intensity)
+# VALUES (, '2022-10-04', 1),
+#        (, '2022-12-31', 10),
+#        (, '2023-02-04', 7),
+#        (, '2023-02-14', 8),
+#        (, '2023-03-02', 4);
 
 
 ##########################################
@@ -301,13 +300,14 @@ VALUES ('Facebook'),
        ('Instagram'),
        ('LinkedIn'),
        ('YouTube');
-       
-INSERT INTO sm_transaction (core_id, smp_id, price_paid) 
-VALUES (11, 11, 100.00), 
-       (11, 12, 150.00),
-       (12, 13, 200.00),
-       (13, 14, 250.00),
-       (14, 15, 300.00);
+
+# fixme need to sort out normalization vs efficiency on this
+# INSERT INTO sm_transaction (core_id, smp_id,sm_item_by_seller_id, price_paid, purchase_date)
+# VALUES (11, 11, 100.00),
+#        (11, 12, 150.00),
+#        (12, 13, 200.00),
+#        (13, 14, 250.00),
+#        (14, 15, 300.00);
 
 INSERT INTO sm_item_type (type)
 VALUES ('Retail'),
@@ -330,12 +330,12 @@ VALUES ('Fashion Retail Co.'),
 ('Beauty Box'),
 ('Furniture Emporium');
 
-INSERT INTO sm_item_by_seller (sm_transaction_id, sm_seller_id, sm_item_id) 
-VALUES (1, 1, 1), 
-       (2, 2, 2), 
-       (3, 3, 3), 
-       (4, 4, 4), 
-       (5, 5, 5);
+INSERT INTO sm_item_by_seller (sm_seller_id, sm_item_id, price)
+VALUES (1, 1, 10.00),
+       (2, 2, 22.99),
+       (3, 3, 33.99),
+       (4, 4, 44.99),
+       (5, 5, 55.99);
 
 INSERT INTO social_media (core_id, sm_platform_id, user_guid, url, last_active)
 VALUES (11, 11,'54966999-feb6-4de4-ae32-3bcad7ed7067', 'https://www.facebook.com/johndoe', '2022-01-01'),
