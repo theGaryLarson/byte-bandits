9b416a72388b447890e6e9598b6ccaa248a79187
theGaryLarson <gary.larson316@gmail.com>
2023-03-12 18:14:21 -0700

    working inserts into data to get around secure_file_priv issue

---
+++ final_presentation/py/load_BO_user_data.py
@@ -0,0 +1,80 @@
+import mysql.connector
+import json
+from vm_config import host, user, password, database
+
+# Load JSON data from file
+with open('../json/bendover_data_feed.json', 'r') as f:
+    data = json.load(f)
+
+# Connect to MySQL database
+db = mysql.connector.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=database
+)
+
+# Create cursor object to execute SQL queries
+cursor = db.cursor()
+
+# Loop through each person's information and insert it into the MySQL table
+for person in data["data"]:
+    cursor = db.cursor()
+    sql = "INSERT INTO bendover_data_feed (" \
+          "bendover_id," \
+          "first_name," \
+          "last_name," \
+          "email," \
+          "political_affiliation," \
+          "political_intensity," \
+          "religious_affiliation," \
+          "religious_intensity," \
+          "social_issue_views," \
+          "social_mate_preference," \
+          "gender," \
+          "birthdate," \
+          "location," \
+          "education," \
+          "occupation," \
+          "hobbies) " \
+          "VALUES (" \
+          "%s, " \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s," \
+          "%s)"
+    values = (  # key is the value in json
+        person["id"],
+        person["first_name"],
+        person["last_name"],
+        person["email"],
+        person["political_affiliation"],
+        person["political_intensity"],
+        person["religious_affiliation"],
+        person["religious_intensity"],
+        json.dumps(person["social_issues"]),
+        json.dumps(person["social_mate_preference"]),
+        person["gender"],
+        person["birthdate"],
+        person["location"],
+        person["education"],
+        person["occupation"],
+        json.dumps(person["hobbies"])
+    )
+    print(values)
+    cursor.execute(sql, values)
+    db.commit()
+
+# Close the database connection
+db.close()
